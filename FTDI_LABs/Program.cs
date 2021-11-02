9d00eae9fd76e8e50873151fc3854121b57fc2ac
SkyVVorker2770 <SkyVVorker2770@yandex.ru>
2021-11-02 22:01:39 +0300

    Добавлен вывод информации о поключенной микросхеме FTDI

--- FTDI_LABs/Program.cs
+++ FTDI_LABs/Program.cs
@@ -7,26 +7,34 @@ namespace FTDI_LABs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             FTDI ftdiDev0 = new FTDI();
             FTDI.FT_STATUS fT_STATUS = FTDI.FT_STATUS.FT_OK;
             byte[] data = new byte[1] { 0 };
             UInt32 numBytesWritten = 0;
 
+           UInt32 devID = 0;
+
+            string devDes = null;
+
             data[0] = 0b11111110;
 
             fT_STATUS = ftdiDev0.OpenByIndex(0);
-            //myFtdiDevice.SetBaudRate(11520);
+   
 
             fT_STATUS = ftdiDev0.SetBitMode(0xFF, FTDI.FT_BIT_MODES.FT_BIT_MODE_ASYNC_BITBANG);
 
             fT_STATUS = ftdiDev0.Write(data, data.Length, ref numBytesWritten);
 
+            ftdiDev0.GetDeviceID(ref devID);
+            ftdiDev0.GetDescription(out devDes);
+            
+            
+
 
             if (fT_STATUS == 0)
             {
                 Console.WriteLine("FTDI Status: Ok!");
+                Console.WriteLine($"FTDI ID: {devID}");
             }
 
         }

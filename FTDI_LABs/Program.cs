dcdcf9860fcc0b3d99c83d0eb7611566cc9c49ac
SkyVVorker2770 <SkyVVorker2770@yandex.ru>
2021-11-05 00:51:48 +0300

    Из-за подтвержденных проблем с фирменной программой FT Prog не удается через неё записать данные в микросхему. В частности не удается записать SN в микросхему. Проведены попытки записать данные в EEPROM через драйвер .NET. Пока безуспешно.

--- FTDI_LABs/Program.cs
+++ FTDI_LABs/Program.cs
@@ -17,6 +17,8 @@ namespace FTDI_LABs
 
             string devDes; // Дескриптор устройсва (название)
 
+            uint devNum = 0; // Количество подключенных устройсв
+
             Console.WriteLine("Введите коимбинацию 00, 01, 10 или 11");
 
             // Консольный ввод и запись полученных данных для отправку в порт FT232H
@@ -39,10 +41,65 @@ namespace FTDI_LABs
                     data[0] = 0b00000000;
                     break;
             }
+            Console.WriteLine(" ");
+
+            ftdiDev0.GetNumberOfDevices(ref devNum); // Получение количества обнаруженных устройств FTDI
+
+            Console.WriteLine($"Количество обнаруженных USB устройств FTDI: {devNum} \n");
+
+            if (devNum > 0)
+            {
+                FTDI.FT_DEVICE_INFO_NODE[] devList = new FTDI.FT_DEVICE_INFO_NODE[devNum]; // Создание листов для хранения информации о подключенных к ПК FTDI
+
+                fT_STATUS = ftdiDev0.OpenByIndex(0); // Метод открывает одно из подключенных устройств по его названию
+
+                fT_STATUS = ftdiDev0.GetDeviceList(devList); // Заполнение нескольких списков инормацией о устрасвах
+
+
+                Console.WriteLine("Информация о устройстве: ");
+
+                
+
+                //FTDI.FT232H_EEPROM_STRUCTURE ee = new FTDI.FT232H_EEPROM_STRUCTURE();
+
+                
+                
+                //ee.SerNumEnable = true;
+
+                //ee.SerialNumber = "FTDI_12345679";
+
+                
+
+                //ftdiDev0.WriteFT232HEEPROM(ee);
+
+                
+
+
+                //ftdiDev0.WriteFT232HEEPROM()
+
+
+
+
+                ftdiDev0.GetSerialNumber(out devList[0].SerialNumber);
+                Console.WriteLine($"Серийный номер устройсва: {devList[0].SerialNumber}");
+                
+
+                ftdiDev0.GetDeviceID(ref devList[0].ID); // 67330068
+                Console.WriteLine($"ID : {devList[0].ID}");
+
+
+                ftdiDev0.GetDeviceType(ref devList[0].Type); // FT_DEVICE_232H
+                Console.WriteLine($"Тип устройсва : {devList[0].Type}");
+
+                ftdiDev0.GetDescription(out devList[0].Description); // Single RS232-HS
+                Console.WriteLine($"Description : {devList[0].Description}");
+
+                Console.WriteLine(" ");
+
+            }
+
 
             
-
-            fT_STATUS = ftdiDev0.OpenByIndex(0); // Метод открывает одно из подключенных устройств по его названию
 
             if (fT_STATUS == FTDI.FT_STATUS.FT_OK)
             {
@@ -59,18 +116,7 @@ namespace FTDI_LABs
             // Запись данных в микросхему и вывод их в порт
             fT_STATUS = ftdiDev0.Write(data, data.Length, ref numBytesWritten);
 
-            ftdiDev0.GetDeviceID(ref devID);
-            ftdiDev0.GetDescription(out devDes);
-            
-            
 
-
-            if (fT_STATUS == 0)
-            {
-                Console.WriteLine("FTDI Status: Ok!");
-                Console.WriteLine($"FTDI ID: {devID}");
-                Console.WriteLine($"FTDI дескриптор: {devDes}");
-            }
 
         }
     }
